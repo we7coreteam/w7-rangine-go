b28b5c5ea36e3b72c8514b5b44735a0b0db19bee
rxwyun <1120309488@qq.com>
2023-02-20 11:12:53 +0800

    优化response

--- src/http/response/response.go
+++ src/http/response/response.go
@@ -6,6 +6,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Formatter func(ctx *gin.Context, data interface{}, error interface{}, statusCode int) map[string]any
+
+var responseFormatter Formatter = func(ctx *gin.Context, data interface{}, error interface{}, statusCode int) map[string]any {
+	return gin.H{
+		"data": data,
+		"code": statusCode,
+		"msg":  error,
+	}
+}
+
+func SetResponseFormatter(formatter Formatter) {
+	responseFormatter = formatter
+}
+
+func GetResponseFormatter() Formatter {
+	return responseFormatter
+}
+
 type Response struct {
 }
 
@@ -28,14 +46,9 @@ func (response *Response) JsonResponseWithError(ctx *gin.Context, err interface{
 	default:
 		response.JsonResponse(ctx, "", err, statusCode)
 	}
-
 }
 
 func (response *Response) JsonResponse(ctx *gin.Context, data interface{}, error interface{}, statusCode int) {
-	ctx.JSON(statusCode, gin.H{
-		"data": data,
-		"code": statusCode,
-		"msg":  error,
-	})
+	ctx.JSON(statusCode, GetResponseFormatter()(ctx, data, error, statusCode))
 	ctx.Abort()
 }
