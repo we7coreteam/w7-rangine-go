a912fb3faff119fb70d12f4ceaee9deaa6c4979b
rxwyun <1120309488@qq.com>
2023-02-21 11:56:04 +0800

    init err_handler

---
+++ src/core/error/err_handler.go
@@ -0,0 +1,53 @@
+package error
+
+import (
+	"github.com/pkg/errors"
+)
+
+type ErrHandler struct {
+	err error
+}
+
+type ErrCustomerHandler func(err error)
+type FinallyHandler func(err error)
+
+func (errHandler ErrHandler) Is(target ...error) bool {
+	for _, arg := range target {
+		if errors.Is(errHandler.err, arg) {
+			return true
+		}
+	}
+	return false
+}
+
+func Throw(message string, previous error) error {
+	var err error
+	if previous == nil {
+		err = errors.New(message)
+	} else {
+		err = errors.Wrap(previous, message)
+	}
+
+	return err
+}
+
+func (errHandler ErrHandler) Found() bool {
+	if errHandler.err != nil {
+		return true
+	}
+	return false
+}
+
+func Try(err error) *ErrHandler {
+	return &ErrHandler{err: err}
+}
+
+func (errHandler ErrHandler) Catch(err error, handler ErrCustomerHandler) {
+	if errors.Is(errHandler.err, err) {
+		handler(errHandler.err)
+	}
+}
+
+func (errHandler ErrHandler) Finally(handler FinallyHandler) {
+	handler(errHandler.err)
+}
