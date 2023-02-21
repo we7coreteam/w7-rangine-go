513deb49682329c90fe6aa4faf3fd32585e30810
rxwyun <1120309488@qq.com>
2023-02-21 19:16:11 +0800

    init server

--- src/http/server/server.go
+++ src/http/server/server.go
@@ -1,16 +1,10 @@
 package server
 
 import (
-	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"github.com/we7coreteam/w7-rangine-go/src/http/session"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
 	"strconv"
-	"time"
 )
 
 var GHttpServer *Server
@@ -48,31 +42,7 @@ func (server *Server) Start() {
 	if err != nil {
 		panic(err)
 	}
+	err = server.GinEngine.Run(serverConfig.Host + ":" + strconv.Itoa(serverConfig.Port))
 
-	srv := &http.Server{
-		Addr:    serverConfig.Host + ":" + strconv.Itoa(serverConfig.Port),
-		Handler: server.GinEngine.Handler(),
-	}
-
-	go func() {
-		// 服务连接
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
-
-	// 等待中断信号以优雅地关闭服务器（
-	quit := make(chan os.Signal)
-	signal.Notify(quit, os.Interrupt)
-	<-quit
-	log.Println("Shutdown Server ...")
-
-	//设置 5 秒的超时时间
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server Shutdown:", err)
-	}
-	log.Println("Server exiting")
+	panic(err)
 }
