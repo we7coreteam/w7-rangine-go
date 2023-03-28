21323354953cc2d3ae8857c558fd999693f17436
rxwyun <1120309488@qq.com>
2023-03-28 19:27:46 +0800

    优化 provider

--- src/components/redis/provider.go
+++ src/components/redis/provider.go
@@ -9,16 +9,16 @@ type Provider struct {
 }
 
 func (provider *Provider) Register() {
-	err := provider.GetContainer().NamedSingleton("redis-factory", func() *Factory {
-		var redisConfigMap map[string]Config
-		err := provider.GetConfig().UnmarshalKey("redis", &redisConfigMap)
-		if err != nil {
-			panic(err)
-		}
+	var redisConfigMap map[string]Config
+	err := provider.GetConfig().UnmarshalKey("redis", &redisConfigMap)
+	if err != nil {
+		panic(err)
+	}
 
-		factory := NewRedisFactory()
-		factory.Register(redisConfigMap)
+	factory := NewRedisFactory()
+	factory.Register(redisConfigMap)
 
+	err = provider.GetContainer().NamedSingleton("redis-factory", func() *Factory {
 		return factory
 	})
 	if err != nil {
