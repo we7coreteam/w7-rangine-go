79c799f3eea678e9fe7a0ab3c3e33304cdb27a5d
rxwyun <1120309488@qq.com>
2023-03-28 16:17:59 +0800

    优化 session config

--- src/http/session/config.go
+++ src/http/session/config.go
@@ -18,8 +18,3 @@ type SessionConf struct {
 	Lifetime    time.Duration `mapstructure:"life_time" json:"life_time" yaml:"life_time"`
 	IdleTimeout time.Duration `mapstructure:"idle_time" json:"idle_time" yaml:"idle_time"`
 }
-
-type Config struct {
-	Session SessionConf `mapstructure:"session" json:"session" yaml:"session"`
-	Cookie  Cookie      `mapstructure:"cookie" json:"cookie" yaml:"cookie"`
-}
