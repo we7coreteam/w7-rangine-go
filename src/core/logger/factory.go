0d054b1395ba2fbbd6f20e30ab69e07cf52de504
rxwyun <1120309488@qq.com>
2023-04-07 14:40:00 +0800

    update

--- src/core/logger/factory.go
+++ src/core/logger/factory.go
@@ -102,7 +102,7 @@ func (factory *Factory) MakeLogger(level zapcore.Level, ws ...zapcore.WriteSynce
 		atomicLevel,                           // 日志级别
 	)
 
-	return zap.New(core, zap.AddCaller(), zap.AddCallerSkip(4), zap.AddStacktrace(zap.FatalLevel+1))
+	return zap.New(core, zap.AddCaller(), zap.AddCallerSkip(0), zap.AddStacktrace(zap.ErrorLevel))
 }
 
 func (factory *Factory) Channel(channel string) (*zap.Logger, error) {
