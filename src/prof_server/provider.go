09fc5d3de77e2b71c06314dc1cca47a89a073072
rxwyun <1120309488@qq.com>
2023-04-12 15:58:41 +0800

    添加prof server

---
+++ src/prof_server/provider.go
@@ -0,0 +1,14 @@
+package prof_server
+
+import (
+	"github.com/we7coreteam/w7-rangine-go/src/core/provider"
+	"github.com/we7coreteam/w7-rangine-go/src/core/server"
+)
+
+type Provider struct {
+	provider.Abstract
+}
+
+func (provider *Provider) Register() {
+	server.RegisterServer(NewProfServer(provider.GetConfig()))
+}
