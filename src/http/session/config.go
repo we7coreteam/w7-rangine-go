d7738bce25e299d55d0daccad8f98378feaa9e39
rxwyun <1120309488@qq.com>
2023-04-28 15:12:28 +0800

    update

--- src/http/session/config.go
+++ src/http/session/config.go
@@ -1,11 +1,12 @@
 package session
 
 type Cookie struct {
-	Name     string `mapstructure:"name" json:"name" yaml:"name"`
-	MaxAge   int    `mapstructure:"max_age" json:"max_age" yaml:"max_age"`
-	Domain   string `mapstructure:"domain" json:"domain" yaml:"domain"`
-	HttpOnly bool   `mapstructure:"http_only" json:"http_only" yaml:"http_only"`
-	Path     string `mapstructure:"path" json:"path" yaml:"path"`
-	Secure   bool   `mapstructure:"secure" json:"secure" yaml:"secure"`
-	SameSite int    `mapstructure:"same_site" json:"same_site" yaml:"same_site"`
+	Name     string         `mapstructure:"name" json:"name" yaml:"name"`
+	MaxAge   int            `mapstructure:"max_age" json:"max_age" yaml:"max_age"`
+	Domain   string         `mapstructure:"domain" json:"domain" yaml:"domain"`
+	HttpOnly bool           `mapstructure:"http_only" json:"http_only" yaml:"http_only"`
+	Path     string         `mapstructure:"path" json:"path" yaml:"path"`
+	Secure   bool           `mapstructure:"secure" json:"secure" yaml:"secure"`
+	SameSite int            `mapstructure:"same_site" json:"same_site" yaml:"same_site"`
+	Options  map[string]any `mapstructure:"options" json:"options" yaml:"options"`
 }
--- src/prof/config.go
+++ src/prof/config.go
@@ -1,6 +1,7 @@
 package prof
 
 type Config struct {
-	Host string `mapstructure:"host" json:"host" yaml:"host"`
-	Port string `mapstructure:"port" json:"port" yaml:"port"`
+	Host    string         `mapstructure:"host" json:"host" yaml:"host"`
+	Port    string         `mapstructure:"port" json:"port" yaml:"port"`
+	Options map[string]any `mapstructure:"options" json:"options" yaml:"options"`
 }
