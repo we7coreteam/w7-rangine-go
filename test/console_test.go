894acc3621fa68b24165143721b62f348da01c34
rxwyun <1120309488@qq.com>
2023-09-15 17:37:03 +0800

    添加command默认参数测试

--- test/console_test.go
+++ test/console_test.go
@@ -3,11 +3,29 @@ package test
 import (
 	"github.com/spf13/cobra"
 	"github.com/we7coreteam/w7-rangine-go/src/console"
+	"os"
 	"testing"
 )
 
 func init() {
 	cobra.EnableCommandSorting = false
+}
+
+type DefaultArgCommand struct {
+	console.Abstract
+}
+
+func (c DefaultArgCommand) GetName() string {
+	return "default:args"
+}
+
+func (c DefaultArgCommand) Configure(cmd *cobra.Command) {
+	cmd.Flags().String("test-arg", "", "test arg")
+}
+
+func (c DefaultArgCommand) Handle(cmd *cobra.Command, args []string) {
+	str, _ := cmd.Flags().GetString("test-arg")
+	os.Setenv("TEST_COMMAND_ARG", str)
 }
 
 func TestAddCommand(t *testing.T) {
@@ -23,5 +41,40 @@ func TestAddCommand(t *testing.T) {
 	}
 	if consoleManager.GetHandler().Commands()[1].Use != "make:model" {
 		t.Error("command make model register fail")
+	}
+}
+
+func TestDefaultArgs(t *testing.T) {
+	err := os.Unsetenv("TEST_COMMAND_ENV")
+	if err != nil {
+		t.Error(err)
+	}
+	oldEnv := os.Getenv("TEST_COMMAND_ENV")
+	os.Args = []string{"cobra.test", "-eTEST_COMMAND_ENV=12"}
+	console.NewConsole()
+
+	newEnv := os.Getenv("TEST_COMMAND_ENV")
+	if oldEnv == newEnv || newEnv != "12" {
+		t.Error("command default args err")
+	}
+
+	err = os.Unsetenv("TEST_COMMAND_ENV")
+	if err != nil {
+		t.Error(err)
+	}
+	oldEnv = os.Getenv("TEST_COMMAND_ENV")
+	os.Args = []string{"text", "default:args", "-eTEST_COMMAND_ENV=14", "--test-arg=test"}
+	consoleManager := console.NewConsole()
+	consoleManager.RegisterCommand(&DefaultArgCommand{})
+	consoleManager.Run()
+
+	newEnv = os.Getenv("TEST_COMMAND_ENV")
+	if oldEnv == newEnv || newEnv != "14" {
+		t.Error("command default args err")
+	}
+
+	argEnv := os.Getenv("TEST_COMMAND_ARG")
+	if argEnv != "test" {
+		t.Error("command default args err")
 	}
 }
