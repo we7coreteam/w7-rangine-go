2232484ff48066c1cf8a92d1aef3b07979fce695
rxwyun <1120309488@qq.com>
2023-09-19 14:30:23 +0800

    修改database config

--- src/components/database/config.go
+++ src/components/database/config.go
@@ -2,11 +2,11 @@ package database
 
 type Config struct {
 	Driver        string         `mapstructure:"driver" json:"driver" yaml:"driver" binding:"required"`
-	Host          string         `mapstructure:"host" json:"host" yaml:"host" binding:"required"`
-	Port          int            `mapstructure:"port" json:"port" yaml:"port" binding:"required"`
-	Username      string         `mapstructure:"user_name" json:"user_name" yaml:"user_name" binding:"required"`
-	Password      string         `mapstructure:"password" json:"password" yaml:"password" binding:"required"`
-	DbName        string         `mapstructure:"db_name" json:"db_name" yaml:"db_name" binding:"required"`
+	Host          string         `mapstructure:"host" json:"host" yaml:"host" binding:"required_without=DSN"`
+	Port          int            `mapstructure:"port" json:"port" yaml:"port" binding:"required_without=DSN"`
+	Username      string         `mapstructure:"user_name" json:"user_name" yaml:"user_name" binding:"required_without=DSN"`
+	Password      string         `mapstructure:"password" json:"password" yaml:"password" binding:"required_without=DSN"`
+	DbName        string         `mapstructure:"db_name" json:"db_name" yaml:"db_name" binding:"required_without=DSN"`
 	Charset       string         `mapstructure:"charset" json:"charset" yaml:"charset"`
 	Prefix        string         `mapstructure:"prefix" json:"prefix" yaml:"prefix"`
 	MaxIdleConn   int            `mapstructure:"max_idle_conn" json:"max_idle_conn" yaml:"max_idle_conn"`
